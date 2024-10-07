5d24a10819e42aaabea13c323a0ce6a7ce6e1ac3
dana367 <dana.larina1@gmail.com>
2024-10-07 16:23:29 +0100

    backend: Add more tests for the member API
    These tests check if the password is correctly changed to a new one in cases
    where the password is forgotten or needs to be updated.

--- backend/tests/blueprints/test_members.py
+++ backend/tests/blueprints/test_members.py
@@ -2,20 +2,21 @@ import pytest
 from freezegun import freeze_time
 from itsdangerous import URLSafeTimedSerializer
 from quart import Quart
+from quart_auth import authenticated_client
 
-from backend.blueprints.members import EMAIL_VERIFICATION_SALT
+from backend.blueprints.members import EMAIL_VERIFICATION_SALT, FORGOTTEN_PASSWORD_SALT
 
 
 async def test_register(app: Quart, caplog: pytest.LogCaptureFixture) -> None:
     test_client = app.test_client()
     data = {
-        "email": "new@tozo.dev",
-        "password": "testPassword2$",
+        "email": "new_dev3@tozo.dev",
+        "password": "testPassword5$",
     }
     await test_client.post("/members/", json=data)
     response = await test_client.post("/sessions/", json=data)
     assert response.status_code == 200
-    assert "Sending welcome.html to new@tozo.dev" in caplog.text
+    assert "Sending welcome.html to new_dev3@tozo.dev" in caplog.text
 
 
 @pytest.mark.parametrize(
@@ -35,3 +36,35 @@ async def test_verify_email_invalid_token(app: Quart) -> None:
     test_client = app.test_client()
     response = await test_client.put("/members/email/", json={"token": "invalid"})
     assert response.status_code == 400
+
+
+async def test_change_password(app: Quart, caplog: pytest.LogCaptureFixture) -> None:
+    test_client = app.test_client()
+    async with authenticated_client(test_client, "2"):
+        response = await test_client.put(
+            "/members/password/",
+            json={
+                "currentPassword": "testPassword5$",
+                "newPassword": "testPassword3$",
+            },
+        )
+        assert response.status_code == 200
+    assert "Sending password_changed.html to new_dev3@tozo.dev" in caplog.text
+
+
+async def test_reset_password(app: Quart, caplog: pytest.LogCaptureFixture) -> None:
+    with freeze_time(None):
+        signer = URLSafeTimedSerializer(app.secret_key, salt=FORGOTTEN_PASSWORD_SALT)
+        token = signer.dumps(2)
+
+    test_client = app.test_client()
+    async with authenticated_client(test_client, "2"):
+        response = await test_client.put(
+            "/members/reset-password/",
+            json={
+                "token": token,
+                "password": "testPassword3$",
+            },
+        )
+        assert response.status_code == 200
+    assert "Sending password_changed.html to new_dev3@tozo.dev" in caplog.text
