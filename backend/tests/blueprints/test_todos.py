0dbfc82aed52a439958f7562ebb3ee1cda10a0ff
dana367 <dana.larina1@gmail.com>
2024-10-08 15:18:27 +0100

    backend: Added additional test for todos. Added test to cover get todos endpoint.

--- backend/tests/blueprints/test_todos.py
+++ backend/tests/blueprints/test_todos.py
@@ -1,3 +1,4 @@
+import pytest
 from quart import Quart
 from quart_auth import authenticated_client
 
@@ -19,6 +20,19 @@ async def test_get_todo(app: Quart) -> None:
         response = await test_client.get("/todos/1/")
         assert response.status_code == 200
         assert (await response.get_json())["task"] == "Test Task"
+
+
+@pytest.mark.parametrize(
+    "complete, expected",
+    [(True, 200), (False, 200), (None, 200)],
+)
+async def test_get_todos(app: Quart, complete: bool | None, expected: int) -> None:
+    test_client = app.test_client()
+    async with authenticated_client(test_client, "1"):  # type: ignore
+        query_string = f"?complete={complete}" if complete is not None else ""
+        response = await test_client.get(f"/todos/{query_string}")
+        assert response.status_code == expected
+        assert isinstance((await response.get_json())["todos"], list)
 
 
 async def test_put_todo(app: Quart) -> None:
